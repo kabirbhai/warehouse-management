470ff12c9212392d424e3e5f7d8bebb778a95b90
kabir <makkobir79@gmail.com>
2022-05-04 00:46:11 +0600

    register and login complited

--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -1,7 +1,28 @@
-import React from "react";
+import { useState } from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
+
 import SocialLogin from "../../SocialLogin/SocialLogin";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  let errorMessage;
+  if (user) {
+    navigate("/home");
+  }
+  if (error) {
+    errorMessage = <p>Wrong password</p>;
+  }
+  // handleLogin
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
   return (
     <>
       <div>
@@ -10,21 +31,29 @@ const Login = () => {
 
       <div className="w-50 mx-auto">
         <SocialLogin />
-        <form className="mt-2">
+        <form onSubmit={handleLogin} className="mt-2">
           <input
             className="w-100 rounded p-2 mb-2"
             type="email"
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="email"
           />
           <br />
           <input
             className="w-100 rounded p-2 mb-2"
             type="password"
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="password"
           />
           <br />
-          <input className="d-block mx-auto mb-2" type="submit" value="Login" />
+          <input
+            onClick={() => signInWithEmailAndPassword(email, password)}
+            className="d-block mx-auto mb-2"
+            type="submit"
+            value="Login"
+          />
         </form>
+        {errorMessage}
       </div>
     </>
   );
--- src/pages/Login/Register/Register.js
+++ src/pages/Login/Register/Register.js
@@ -1,32 +1,58 @@
 import React from "react";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  if (user) {
+    navigate("/home");
+  }
+  // handleRegister
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    createUserWithEmailAndPassword(email, password);
+  };
   return (
     <>
       <div>
         <h1 className="my-2 text-center">Please Register</h1>
       </div>
+
       <div className="w-50 mx-auto">
-        <form className="mt-2">
+        <form onSubmit={handleRegister} className="mt-2">
           <input
             className="w-100 rounded p-2 mb-2"
-            type="name"
+            type="text"
+            name="name"
             placeholder="Name"
           />
           <br />
           <input
             className="w-100 rounded p-2 mb-2"
             type="email"
+            name="email"
             placeholder="email"
           />
           <br />
           <input
             className="w-100 rounded p-2 mb-2"
             type="password"
+            name="password"
             placeholder="password"
           />
           <br />
-          <input className="d-block mx-auto mb-2" type="submit" value="Login" />
+          <input
+            className="d-block mx-auto mb-2"
+            type="submit"
+            value="Register"
+          />
         </form>
       </div>
     </>
