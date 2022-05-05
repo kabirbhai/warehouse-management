6f13dda665c8a29057ace13db8925f8da2e6115e
kabir <makkobir79@gmail.com>
2022-05-06 02:36:14 +0600

    client section added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./Home.css";
 import StokeItems from "../StokeItems/StokeItems";
 import Banner from "../../Banner/Banner";
+import OutClient from "../../OurClient/OutClient";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <StokeItems />
+      <OutClient />
     </div>
   );
 };
---
+++ src/pages/OurClient/OutClient.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./OurClient.css";
+import img1 from "../../images/client/client1.png";
+import img2 from "../../images/client/client2.png";
+import img3 from "../../images/client/client3.png";
+import img4 from "../../images/client/client4.png";
+
+const OutClient = () => {
+  return (
+    <div className="container mb-4">
+      <h1 className="text-center my-4 text-danger">
+        We have partnered with the best shipping carriers
+      </h1>
+      <div className="client-container">
+        <img src={img1} alt="" />
+        <img src={img2} alt="" />
+        <img src={img3} alt="" />
+        <img src={img4} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default OutClient;
