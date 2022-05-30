e29ada6594a63769a3b50e5d02282d133827d2a0
kabir <makkobir79@gmail.com>
2022-05-30 23:05:32 +0600

    new future added

--- src/pages/Home/Section/Products/Products.js
+++ src/pages/Home/Section/Products/Products.js
@@ -5,14 +5,14 @@ import "./Products.css";
 const Products = () => {
   const [items] = useItems([]);
   return (
-    <>
+    <section>
+      <h1 className="text-center my-4 text-primary ">OUR PRODUCTS</h1>
       <div className="product-container container">
         {items.map((product) => (
           <Product key={product._id} product={product}></Product>
         ))}
       </div>
-      <hr />
-    </>
+    </section>
   );
 };
 
--- src/pages/Home/Section/WhatNew/WhatNew.js
+++ src/pages/Home/Section/WhatNew/WhatNew.js
@@ -1,25 +1,33 @@
 import React from "react";
 import "./WhatNew.css";
-import img1 from "../../../../images/banner/2.jpg";
+import img1 from "../../../../images/future1.jpg";
+import img2 from "../../../../images/future2.jpg";
 
 const WhatNew = () => {
   return (
-    <div className="container">
-      <h2 className="text-center text-danger my-4">What's new our WAREHOUSE</h2>
+    <section className="container">
+      <hr />
+      <h2 className="text-center text-primary fw-bold my-4">
+        WHAT'S NEW OUR WAREHOUSE
+      </h2>
       <div className="what-new-container md:flex-wrap">
-        <div className="border">
-          <img className="img-fluid" src={img1} alt="" />
-          <p>We are adding some new collection.</p>
-          <button>See whats new now</button>
+        <div className="border rounded-3 ">
+          <img className="img-fluid rounded" src={img1} alt="" />
+          <p className="text-center ">We are adding some new collection.</p>
+          <button className="btn btn-primary d-block mx-auto mb-2  ">
+            See whats new now
+          </button>
         </div>
-        <div className="border">
-          <img className="img-fluid" src={img1} alt="" />
-          <p>We are adding some new collection.</p>
+        <div className="border rounded-3 ">
+          <img className="img-fluid rounded" src={img2} alt="" />
+          <p className="text-center ">We are adding some new collection.</p>
 
-          <button>See whats new now</button>
+          <button className="btn btn-primary d-block mx-auto mb-2  ">
+            See whats new now
+          </button>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
